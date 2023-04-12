e85c41d71cad7b5488583bdbd004d5c72af2cc28
MaksimCPP8 <maksgreat1978@gmail.com>
2023-04-12 23:24:11 +0400

    Add Project 2.2

---
+++ Lesson 2/Project 2.2/Project 2.2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+
+struct bank_account
+{
+    long int account_number;
+    std::string holder_name;
+    double balance;
+};
+void change_balance(bank_account& m, double new_balance) 
+{
+    m.balance = new_balance;
+}
+
+int main()
+{
+    setlocale(0, "");
+    std::cout << "\n\t\tЗадача 2. Счета.\n";
+
+    long int account;
+    std::string name;
+    double money, new_balance;
+
+    std::cout << "\nВведите имя владельца счёта: ";
+    std::cin >> name;
+    std::cout << "\nВведите номер счёта: ";
+    std::cin >> account;
+    std::cout << "\nВведите баланс счёта: ";
+    std::cin >> money;
+
+    bank_account holder;
+    holder.holder_name = name;
+    holder.account_number = account;
+    holder.balance = money;
+
+    std::cout << "\nВведите новый баланс счёта: ";
+    std::cin >> new_balance;
+
+    change_balance(holder, new_balance);
+
+    std::cout << "\nВаш счёт: " << holder.holder_name << ", " << holder.account_number << ", " << holder.balance << "\n";
+
+}
+

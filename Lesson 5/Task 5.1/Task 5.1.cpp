bb376d7c0798b4bb6771b11e85e0beeedd7a0fe2
MaksimCPP8 <maksgreat1978@gmail.com>
2023-05-05 20:33:11 +0400

    Added Task1 and Task 2 to the Lesson 5 folder

---
+++ Lesson 5/Task 5.1/Task 5.1.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <string>
+
+class Figure
+{
+protected:
+    int sides_count;
+    std::string name;
+
+public:
+    Figure(int count)
+    {
+        sides_count = count;
+    }
+    int get_sides_count()
+    {
+        return sides_count;
+    }
+    std::string get_name()
+    {
+        name = "Фигура: ";
+        return name;
+    }
+};
+
+class Triangle : public Figure
+{
+public:
+    Triangle (int count):Figure(count)
+    {
+
+    }
+    std::string get_name()
+    {
+        name = "Треугольник: ";
+        return name;
+    }
+};
+
+class Quadrangle :public Figure
+{
+public:
+    Quadrangle(int count) :Figure(count)
+    {
+
+    }
+    std::string get_name()
+    {
+        name = "Четырёхугольник: ";
+        return name;
+    }
+};
+
+int main()
+{
+    setlocale(0, "");
+    std::cout << "\n\t\tЗадача 1. Фигуры. Количество сторон.\n\n";
+
+    std::cout << "Количество сторон:\n";
+
+    Figure Noname(0);
+    std::cout << Noname.get_name() << Noname.get_sides_count() << "\n";
+
+    Triangle Triangle(3);
+    std::cout << Triangle.get_name() << Triangle.get_sides_count() << "\n";
+
+    Quadrangle Quadrangle(4);
+    std::cout << Quadrangle.get_name() << Quadrangle.get_sides_count() << "\n";
+
+}
---
+++ Lesson 5/Task 5.2/Task 5.2.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <string>
+
+class Triangle
+{
+protected:
+    int side_a;
+    int side_b;
+    int side_c;
+
+    int angle_A;
+    int angle_B;
+    int angle_C;
+
+    std::string figure_name;
+
+public:
+    Triangle(int a, int b, int c, int A, int B, int C)
+    {
+        side_a = a;
+        side_b = b;
+        side_c = c;
+        angle_A = A;
+        angle_B = B;
+        angle_C = C;
+    }
+    std::string get_name()
+    {
+        figure_name = "Треугольник: ";
+        return figure_name;
+    }
+    int get_side_a()
+    {
+        return side_a;
+    }
+    int get_side_b()
+    {
+        return side_b;
+    }
+    int get_side_c()
+    {
+        return side_c;
+    }
+    int get_angle_A()
+    {
+        return angle_A;
+    }
+    int get_angle_B()
+    {
+        return angle_B;
+    }
+    int get_angle_C()
+    {
+        return angle_C;
+    }
+};
+
+void print_info(Triangle* p)
+{
+    std::cout << "\n" << p.get_name << "\n";
+    std::cout << "Стороны:" << " a=" << p.get_side_a << " b=" << p.get_side_b << " c=" << p.get_side_c << "\n";
+    std::cout << "Углы:" << " A=" << p.get_angle_A << " B=" << p.get_angle_B << " C=" << p.get_angle_C << "\n";
+}
+
+int main()
+{
+    setlocale(0, "");
+    std::cout << "\n\t\tЗадача 2. Фигуры. Стороны и углы.\n";
+      
+    Triangle Triangle(10, 20, 30, 50, 60, 70);
+    print_info(&Triangle);
+}
+

4d864680ce2603674b14b47dbe0db479f04dd6c1
MaksimCPP8 <maksgreat1978@gmail.com>
2023-05-13 21:51:14 +0400

    Work has been done on the errors in Task 5.3 in the Lesson 5 folder

--- Lesson 5/Task 5.3/Task 5.3.cpp
+++ Lesson 5/Task 5.3/Task 5.3.cpp
@@ -12,30 +12,30 @@ public:
     {
 
     }
-    Figure(std::string figure_name, int quantity_sides)
+    Figure(int quantity_sides)
     {
-        this->figure_name = figure_name;
+        figure_name = "Фигура:";
         this->quantity_sides= quantity_sides;
     }
     std::string get_name()
     {
         return figure_name;
     }
-    virtual void check()
+    virtual std::string check()
     {
         if (!quantity_sides)
         {
-            std::cout << "Правильная\n";
+            return "Правильная";
         }
         else 
         {
-            std::cout << "Неправильная\n";
+            return "Неправильная";
         }
     }
     virtual void print_myself()
     {
         std::cout << "\n" << get_name() << "\n";
-        check();
+        std::cout << check() << "\n";
         std::cout << "Количество сторон: " << quantity_sides << "\n";
     };
    
@@ -57,7 +57,7 @@ public:
     {
 
     }
-    Triangle(std::string figure_name, int quantity_sides, int a, int b, int c, int A, int B, int C)
+    Triangle(int quantity_sides, int a, int b, int c, int A, int B, int C)
     {
         side_a = a;
         side_b = b;
@@ -67,7 +67,7 @@ public:
         angle_B = B;
         angle_C = C;
 
-        this->figure_name = figure_name;
+        figure_name = "Треугольник:";
         this->quantity_sides = quantity_sides;
 
     }
@@ -99,21 +99,21 @@ public:
     {
         return angle_C;
     }
-    void check()override
+    std::string check() override
     {
         if (quantity_sides==3 && (angle_A + angle_B + angle_C)==180)
         {
-            std::cout << "Правильная\n";
+            return "Правильная";
         }
         else
         {
-            std::cout << "Неправильная\n";
+            return "Неправильная";
         }
     }
     void print_myself() override
     {
         std::cout << "\n" << get_name() << "\n";
-        check();
+        std::cout << check() << "\n";
         std::cout << "Количество сторон: " << quantity_sides << "\n";
         std::cout << "Стороны:" << " a=" << get_side_a() << " b=" << get_side_b() << " c=" << get_side_c() << "\n";
         std::cout << "Углы:" << " A=" << get_angle_A() << " B=" << get_angle_B() << " C=" << get_angle_C() << "\n";
@@ -123,7 +123,7 @@ public:
 class RectTriangle : public Triangle
 {
 public:
-    RectTriangle(std::string figure_name, int quantity_sides, int a, int b, int c, int A, int B)
+    RectTriangle(int quantity_sides, int a, int b, int c, int A, int B)
     {
         side_a = a;
         side_b = b;
@@ -133,22 +133,22 @@ public:
         angle_B = B;
         angle_C = 90;
 
-        this->figure_name = figure_name;
+        figure_name = "Прямоугольный треугольник:";
         this->quantity_sides = quantity_sides;
     }
     std::string get_name()
     {
         return figure_name;
     }
-    void check() override
+    std::string check() override
     {
         if (angle_C == 90)
         {
-            std::cout << "Правильная\n";
+            return "Правильная";
         }
         else
         {
-            std::cout << "Неправильная\n";
+            return "Неправильная";
         }
     }
 };
@@ -156,7 +156,7 @@ public:
 class IsosTriangle :public Triangle
 {
 public:
-    IsosTriangle(std::string figure_name, int quantity_sides, int a, int b, int A, int B)
+    IsosTriangle(int quantity_sides, int a, int b, int A, int B)
     {
         side_a = side_c = a;
         side_b = b;
@@ -164,22 +164,22 @@ public:
         angle_A = angle_C = A;
         angle_B = B;
 
-        this->figure_name = figure_name;
+        figure_name = "Равнобедренный треугольник:";
         this->quantity_sides = quantity_sides;
     }
     std::string get_name()
     {
         return figure_name;
     }
-    void check() override
+    std::string check() override
     {
         if (side_a == side_c && angle_A == angle_C)
         {
-            std::cout << "Правильная\n";
+            return "Правильная";
         }
         else
         {
-            std::cout << "Неправильная\n";
+            return "Неправильная";
         }
     }
 };
@@ -187,28 +187,28 @@ public:
 class EquilTriangle :public Triangle
 {
 public:
-    EquilTriangle(std::string figure_name, int quantity_sides, int a)
+    EquilTriangle(int quantity_sides, int a)
     {
         side_a = side_b = side_c = a;
 
         angle_A = angle_B = angle_C = 60;
 
-        this->figure_name = figure_name;
+        figure_name = "Равносторонний треугольник:";
         this->quantity_sides = quantity_sides;
     }
     std::string get_name()
     {
         return figure_name;
     }
-    void check() override
+    std::string check() override
     {
-        if ((side_a == side_b == side_c) && (angle_A == angle_B == angle_C == 60))
+        if (side_a == side_b && side_b == side_c && side_a == side_c && angle_A == 60 && angle_B == 60 && angle_C == 60)
         {
-            std::cout << "Правильная\n";
-        } 
+            return "Правильная";
+        }
         else
         {
-            std::cout << "Неправильная\n";
+            return "Неправильная";
         }
     }
 };
@@ -231,7 +231,7 @@ public:
     {
 
     }
-    Quadrilateral(std::string figure_name, int quantity_sides, int a, int b, int c, int d, int A, int B, int C, int D)
+    Quadrilateral(int quantity_sides, int a, int b, int c, int d, int A, int B, int C, int D)
     {
         side_a = a;
         side_b = b;
@@ -243,7 +243,7 @@ public:
         angle_C = C;
         angle_D = D;
 
-        this->figure_name = figure_name;
+        figure_name = "Четырёхугольник:";
         this->quantity_sides = quantity_sides;
     }
     std::string get_name()
@@ -282,21 +282,21 @@ public:
     {
         return angle_D;
     }
-    void check() override
+    std::string check() override
     {
         if (quantity_sides == 4 && (angle_A + angle_B + angle_C + angle_D) == 360)
         {
-            std::cout << "Правильная\n";
+            return "Правильная";
         }
         else
         {
-            std::cout << "Неправильная\n";
+            return "Неправильная";
         }
     }
     void print_myself() override
     {
         std::cout << "\n" << get_name() << "\n";
-        check();
+        std::cout << check() << "\n";
         std::cout << "Количество сторон: " << quantity_sides << "\n";
         std::cout << "Стороны:" << " a=" << get_side_a() << " b=" << get_side_b() << " c=" << get_side_c() << " d=" << get_side_d() << "\n";
         std::cout << "Углы:" << " A=" << get_angle_A() << " B=" << get_angle_B() << " C=" << get_angle_C() << " D=" << get_angle_D() << "\n";
@@ -310,7 +310,7 @@ public:
     {
 
     }
-    Parallelogram(std::string figure_name, int quantity_sides,int a, int b, int A, int B)
+    Parallelogram(int quantity_sides,int a, int b, int A, int B)
     {
         side_a = side_c = a;
         side_b = side_d = b;
@@ -318,22 +318,22 @@ public:
         angle_A = angle_C = A;
         angle_B = angle_D = B;
 
-        this->figure_name = figure_name;
+        figure_name = "Параллелограмм:";
         this->quantity_sides = quantity_sides;
     }
     std::string get_name()
     {
         return figure_name;
     }
-    void check() override
+    std::string check() override
     {
         if (side_a == side_c && side_b == side_d && angle_A == angle_C && angle_B == angle_D)
         {
-            std::cout << "Правильная\n";
+            return "Правильная";
         }
         else
         {
-            std::cout << "Неправильная\n";
+            return "Неправильная";
         }
     }
 };
@@ -345,29 +345,29 @@ public:
     {
 
     }
-    Rectangle(std::string figure_name, int quantity_sides, int a, int b)
+    Rectangle(int quantity_sides, int a, int b)
     {
         side_a = side_c = a;
         side_b = side_d = b;
 
         angle_A = angle_B = angle_C = angle_D = 90;
 
-        this->figure_name = figure_name;
+        figure_name = "Прямоугольник:";
         this->quantity_sides = quantity_sides;
     }
     std::string get_name()
     {
         return figure_name;
     }
-    void check() override
+    std::string check() override
     {
-        if (side_a == side_c && side_b == side_d && (angle_A == angle_B == angle_C == angle_D == 90))
+        if (side_a == side_c && side_b == side_d && angle_A == 90 && angle_B == 90 && angle_C == 90 && angle_D == 90)
         {
-            std::cout << "Правильная\n";
+            return "Правильная";
         }
         else
         {
-            std::cout << "Неправильная\n";
+            return "Неправильная";
         }
     }
 };
@@ -375,29 +375,29 @@ public:
 class Rhomb : public Parallelogram
 {
 public:
-    Rhomb(std::string figure_name, int quantity_sides, int a, int A, int B)
+    Rhomb(int quantity_sides, int a, int A, int B)
     {
         side_a = side_b = side_c = side_d = a;
 
         angle_A = angle_C = A;
         angle_B = angle_D = B;
 
-        this->figure_name = figure_name;
+        figure_name = "Ромб:";
         this->quantity_sides = quantity_sides;
     }
     std::string get_name()
     {
         return figure_name;
     }
-    void check() override
+    std::string check() override
     {
-        if ((side_a == side_b == side_c == side_d) && angle_A == angle_C && angle_B == angle_D)
+        if (side_a == side_b && side_b == side_c && side_c == side_d && side_a == side_d && angle_A == angle_C && angle_B == angle_D)
         {
-            std::cout << "Правильная\n";
+            return "Правильная";
         }
         else
         {
-            std::cout << "Неправильная\n";
+            return "Неправильная";
         }
     }
 };
@@ -405,28 +405,28 @@ public:
 class Square : public Rectangle
 {
 public:
-    Square(std::string figure_name, int quantity_sides, int a)
+    Square(int quantity_sides, int a)
     {
         side_a = side_b = side_c = side_d = a;
 
         angle_A = angle_B = angle_C = angle_D = 90;
 
-        this->figure_name = figure_name;
+        figure_name = "Квадрат:";
         this->quantity_sides = quantity_sides;
     }
     std::string get_name()
     {
         return figure_name;
     }
-    void check() override
+    std::string check() override
     {
-        if ((side_a == side_b == side_c == side_d) && (angle_A == angle_B == angle_C == angle_D == 90))
+        if (side_a == side_b && side_b == side_c && side_c == side_d && side_a == side_d && angle_A == 90 && angle_B == 90 && angle_C == 90 && angle_D == 90)
         {
-            std::cout << "Правильная\n";
+            return "Правильная";
         }
         else
         {
-            std::cout << "Неправильная\n";
+            return "Неправильная";
         }
     }
 };
@@ -436,38 +436,53 @@ void print_info(Figure* p)
     p->print_myself();
 }
 
+void check(Figure* p)
+{
+    p->check();
+}
+
 int main()
 {
     setlocale(0, "");
     std::cout << "\n\t\tЗадача 3. Фигуры. Методы.\n";
 
-    Figure Fig("Фигура:", 0);
+    Figure Fig(0);
+    check(&Fig);
     print_info(&Fig);
 
-    Triangle Tr("Треугольник:", 3, 10, 20, 30, 50, 60, 70);
+    Triangle Tr(3, 10, 20, 30, 50, 60, 70);
+    check(&Tr);
     print_info(&Tr);
 
-    RectTriangle RectTr("Прямоугольный треугольник:", 3, 10, 20, 30, 50, 60);
+    RectTriangle RectTr(3, 10, 20, 30, 50, 60);
+    check(&RectTr);
     print_info(&RectTr);
 
-    IsosTriangle IsosTr("Равнобедренный треугольник:", 3, 10, 20, 50, 60);
+    IsosTriangle IsosTr(3, 10, 20, 50, 60);
+    check(&IsosTr);
     print_info(&IsosTr);
 
-    EquilTriangle EquilTr("Равносторонний треугольник:", 3, 30);
+    EquilTriangle EquilTr(3, 30);
+    check(&EquilTr);
     print_info(&EquilTr);
 
-    Quadrilateral Quadr("Четырёхугольник:", 4, 10, 20, 30, 40, 50, 60, 70, 80);
+    Quadrilateral Quadr(4, 10, 20, 30, 40, 50, 60, 70, 80);
+    check(&Quadr);
     print_info(&Quadr);
 
-    Rectangle Rect("Прямоугольник:", 4, 10, 20);
+    Rectangle Rect(4, 10, 20);
+    check(&Rect);
     print_info(&Rect);
 
-    Square Box("Квадрат: ", 4, 20);
+    Square Box(4, 20);
+    check(&Box);
     print_info(&Box);
 
-    Parallelogram Parall("Параллелограмм:", 4, 20, 30, 30, 40);
+    Parallelogram Parall(4, 20, 30, 30, 40);
+    check(&Parall);
     print_info(&Parall);
-
-    Rhomb Rho("Ромб:", 4, 30, 30, 40);
+    
+    Rhomb Rho(4, 30, 30, 40);
+    check(&Rho);
     print_info(&Rho);
 }

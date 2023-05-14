7917b00b4342fc51f0f174425b481a6d6189b80b
MaksimCPP8 <maksgreat1978@gmail.com>
2023-05-14 21:32:21 +0400

    Added Task 7.1, 7.2 to the Lesson 7 folder

---
+++ Lesson 7/Task 7.1/Task 7.1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#define MODE 1
+
+int main()
+{
+    setlocale(0, "");
+    std::cout << "\n\t\tЗадача 1. Препроцессорные директивы.\n";
+
+    int a, b;
+
+#ifndef MODE
+#error MODE is not defined
+#endif
+
+#if MODE == 0
+    std::cout << "\nРаботаю в режиме тренировки.\n";
+#elif MODE == 1
+#define add(a,b)((a)+(b))
+    std::cout << "\nРаботаю в боевом режиме.\n";
+    std::cout << "\nВведите число 1: ";
+    std::cin >> a;
+    std::cout << "\nВведите число 2: ";
+    std::cin >> b;
+    std::cout << "\nРезультат сложения: " << add(a, b) << "\n";
+#else 
+    std::cout << "\nНеизвестный режим. Завершение работы.\n";
+#endif
+}
+
---
+++ Lesson 7/Task 7.2/Task 7.2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#define SUB(a,b)((a)-(b))
+
+int main()
+{
+#ifndef SUB
+#error SUB is not defined
+#endif
+
+    setlocale(0, "");
+    std::cout << "\n\t\tЗадача 2. Макрос SUB.\n\n";
+
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << "\n";
+	std::cout << SUB(a, b) * c << "\n";
+	std::cout << SUB(a, b + c) * c << "\n";
+}
